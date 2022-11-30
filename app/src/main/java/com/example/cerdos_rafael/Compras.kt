53d1925c92e45744731c4060983e993a458ab5c6
graycinod <yuliethvera20022301@gmail.com>
2022-11-30 15:24:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cerdos_rafael/Compras.kt
@@ -0,0 +1,11 @@
+package com.example.cerdos_rafael
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Compras : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compras)
+    }
+}
---
+++ app/src/main/java/com/example/cerdos_rafael/Stadisticas.kt
@@ -0,0 +1,11 @@
+package com.example.cerdos_rafael
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Stadisticas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stadisticas)
+    }
+}
